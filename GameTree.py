8bcd3c2becc6920f0d8796cbe70fdb42783e8a58
Martins <martins.bilinskis@edu.rtu.lv>
2025-03-27 09:39:22 +0200

    Started to fix

--- GameTree.py
+++ GameTree.py
@@ -30,91 +30,92 @@ class TreeNode:
         return hash((self.gameNum, self.p1points, self.p2points, self.turnCount))
 
 
-#  Sāk izveidot koku
-def buildTree(startNumb, depth=3):
-    root = TreeNode(startNumb, 0)
-    createdNodes = set()
-    createLevel(root, createdNodes, depth)  # uzstādīts dzīļums - 3 līmeņi
-    return root
+    #  Sāk izveidot koku
+    def buildTree(self, startNumb, depth=3):
+        root = TreeNode(startNumb, 0)
+        createdNodes = set()
+        TreeNode.createLevel(root, createdNodes, depth)  # uzstādīts dzīļums - 3 līmeņi
+        return root
 
 
-"""
-    Galvenā fukcija, kura ģenerē koku, pēc nosacījumiem noteiktiem nosacījumiem.
-    Tiek izveidots pēcteča mezgls, kuram ir pievienoti spēlētāju iegūtie punkti visos gadījumos
-    Mezgls netiek ģenerēts, ja ir sasniegts maksimālais dotais dziļums vai, ja esošais skaitlis pārsniedz 1200.
-    Pirms jauna mezgla piešķires tiek pārbaudīts vai jau identisks mezgls eksistē, ja
-    eksistē tad piešķir jau eksistējošo mezglu, ja nē tad kokam tiek pievienots jaunais mezgls
-"""
+    """
+        Galvenā fukcija, kura ģenerē koku, pēc nosacījumiem noteiktiem nosacījumiem.
+        Tiek izveidots pēcteča mezgls, kuram ir pievienoti spēlētāju iegūtie punkti visos gadījumos
+        Mezgls netiek ģenerēts, ja ir sasniegts maksimālais dotais dziļums vai, ja esošais skaitlis pārsniedz 1200.
+        Pirms jauna mezgla piešķires tiek pārbaudīts vai jau identisks mezgls eksistē, ja
+        eksistē tad piešķir jau eksistējošo mezglu, ja nē tad kokam tiek pievienots jaunais mezgls
+    """
 
 
-def createLevel(parentNode, createdNodes, maxDepth):
-    # Pārbaude vai dziļums ir sasniegts
-    if parentNode.turnCount >= maxDepth:
-        return
+    def createLevel(parentNode, createdNodes, maxDepth):
+        # Pārbaude vai dziļums ir sasniegts
+        if parentNode.turnCount >= maxDepth:
+            return
 
-    number = parentNode.gameNum
-    # Vai esošais skaitlis ir jau lielāks par 1200
-    if number >= 1200:
-        return
+        number = parentNode.gameNum
+        # Vai esošais skaitlis ir jau lielāks par 1200
+        if number >= 1200:
+            return
 
-    # Ģenerē 3 dažādos variantus
-    nodes = []
-    for i in range(2, 5):
-        nodes.append(TreeNode(number * i, parentNode.turnCount + 1))
+        # Ģenerē 3 dažādos variantus
+        nodes = []
+        for i in range(2, 5):
+            nodes.append(TreeNode(number * i, parentNode.turnCount + 1))
 
-    # Iziet cauri veidotajiem mezgliem
-    for node in nodes:
-        node.p1points = parentNode.p1points
-        node.p2points = parentNode.p2points
+        # Iziet cauri veidotajiem mezgliem
+        for node in nodes:
+            node.p1points = parentNode.p1points
+            node.p2points = parentNode.p2points
 
-        addPoints(node)
-        
-        print(f"Mezgls {node.gameNum} (P1: {node.p1points}, P2: {node.p2points}) ID: {id(node)}") # to var nokomentēt, tas tikai priekš self pārbaudei
+            TreeNode.addPoints(node)
 
-        # Pārbaude vai jau pirmstam ir veidots tāds mezgls, ja nav tad veido jaunu
-        if node not in createdNodes:
-            parentNode.addChild(node)
-            createdNodes.add(node)
-            createLevel(node, createdNodes, maxDepth)
-        # Ja ir tad atrod to un pievieno eksistējošo un nav nepieciešam ģenerēt
+            print(f"Mezgls {node.gameNum} (P1: {node.p1points}, P2: {node.p2points}) ID: {id(node)}") # to var nokomentēt, tas tikai priekš self pārbaudei
+
+            # Pārbaude vai jau pirmstam ir veidots tāds mezgls, ja nav tad veido jaunu
+            if node not in createdNodes:
+                parentNode.addChild(node)
+                createdNodes.add(node)
+                TreeNode.createLevel(node, createdNodes, maxDepth)
+            # Ja ir tad atrod to un pievieno eksistējošo un nav nepieciešam ģenerēt
+            else:
+                for existing_node in createdNodes:
+                    if existing_node == node:
+                        parentNode.addChild(existing_node)
+                        print(f"Mezgls {node.gameNum} (P1: {node.p1points}, P2: {node.p2points}) jau eksistē, ID: {id(existing_node)}") # tas arī
+                        break
+
+
+    # Sistēma kā pievieno punktus vai atņem spēlētājiem spēles laikā.
+    def addPoints(node):
+        if node.turnCount % 2 == 1:
+            if node.gameNum % 2 == 0:
+                node.p2points -= 1
+            else:
+                node.p1points += 1
         else:
-            for existing_node in createdNodes:
-                if existing_node == node:
-                    parentNode.addChild(existing_node)
-                    print(f"Mezgls {node.gameNum} (P1: {node.p1points}, P2: {node.p2points}) jau eksistē, ID: {id(existing_node)}") # tas arī
-                    break
+            if node.gameNum % 2 == 0:
+                node.p1points -= 1
+            else:
+                node.p2points += 1
 
 
-# Sistēma kā pievieno punktus vai atņem spēlētājiem spēles laikā.
-def addPoints(node):
-    if node.turnCount % 2 == 1:
-        if node.gameNum % 2 == 0:
-            node.p2points -= 1
-        else:
-            node.p1points += 1
-    else:
-        if node.gameNum % 2 == 0:
-            node.p1points -= 1
-        else:
-            node.p2points += 1
+    # ģenerē no dota node noteiktā dziļumā tālāk koku
+    def generateLevel(node, depth):
+        TreeNode.createLevel(node, set(), node.turnCount + depth)
 
 
-# ģenerē no dota node noteiktā dziļumā tālāk koku
-def generateLevel(node, depth):
-    createLevel(node, set(), node.turnCount + depth)
-
-
-def printTree(root):
-    for node in root.children:
-        print("--" * node.turnCount + ">" + str(node.gameNum) + f" (P1: {node.p1points}, P2: {node.p2points})")
-        printTree(node)
+    def printTree(root):
+        for node in root.children:
+            print("--" * node.turnCount + ">" + str(node.gameNum) + f" (P1: {node.p1points}, P2: {node.p2points})")
+            TreeNode.printTree(node)
 
 
 if __name__ == '__main__':
     # Izveido koku ar sākotnējo skaitli
-    Tree = buildTree(8)
+    ObjTree = TreeNode(8)
+    Tree = ObjTree.buildTree(8, 3)
     # īslaicīgi, lai ģenerētu citā līmenī tālāk
     izveletais = Tree.children[0].children[0].children[0]
-    generateLevel(izveletais, 3)
+    TreeNode.generateLevel(izveletais, 3)
 
-    printTree(Tree)
+    TreeNode.printTree(Tree)
--- heuristic.py
+++ heuristic.py
@@ -1,114 +1,49 @@
-class TreeNode:
-    def __init__(self, data, depth=0):
-        self.data = data
-        self.parent = None
-        self.children = []
-        self.p1points = 0
-        self.p2points = 0
-        self.bestmove = depth
+import GameTree
+class HeurTreeNode(GameTree.TreeNode):
 
-    def addChild(self, node):
-        self.children.append(node)
-
-    def __eq__(self, other):
-        if not isinstance(other, TreeNode):
-            return NotImplemented
-        return (
-            self.data == other.data and
-            self.p1points == other.p1points and
-            self.p2points == other.p2points and
-            self.bestmove == other.bestmove
-        )
-
-    def __hash__(self):
-        return hash((self.data, self.p1points, self.p2points, self.bestmove))
-    
     def heuristic(self):
-        return (self.p1points - self.p2points) + (1200 - self.data) * 0.01 # Fromula, kura dod koeficentu. (Vel būs uzalbota)
+        return (self.p1points - self.p2points) + (1200 - self.gameNum) * 0.01 # Fromula, kura dod koeficentu. (Vel būs uzalbota)
 
 
-def buildTree(startNumb):
-    root = TreeNode(startNumb, 0)
-    createdNodes = set()
-    createLevel(root, createdNodes, 5)  # Uzstādīts dzīļums - 5 līmeņi, lai varētu dabūt viss atrāko uzvaru priekš P1.
-    return root
+    def ShortWinP1(node, path=[]):
+        path.append((node.gameNum, node.p1points, node.p2points, node.turnCount))
+        if node.gameNum >= 1200 and node.p1points > node.p2points:
+            return path
+
+        best_path = None
+        for child in sorted(node.children, key=lambda x: x.p1points - x.p2points, reverse=True):
+            new_path = HeurTreeNode.ShortWinP1(child, path[:])
+            if new_path and (best_path is None or len(new_path) < len(best_path)):
+                best_path = new_path
+
+        return best_path
 
 
-def createLevel(parentNode, createdNodes, maxDepth):
-    if parentNode.bestmove >= maxDepth:
-        return
-    
-    number = parentNode.data
-    if number >= 1200:
-        return
-    
-    nodes = [TreeNode(number * 2, parentNode.bestmove + 1),
-             TreeNode(number * 3, parentNode.bestmove + 1),
-             TreeNode(number * 4, parentNode.bestmove + 1)]
-
-    for node in nodes:
-        node.p1points = parentNode.p1points
-        node.p2points = parentNode.p2points
-        
-        addPoints(node)
-        
-        if node not in createdNodes:
-            parentNode.addChild(node)
-            createdNodes.add(node)
-            createLevel(node, createdNodes, maxDepth)
-        else:
-            for existing_node in createdNodes:
-                if existing_node == node:
-                    parentNode.addChild(existing_node)
-                    break
+    def printPath(path):
+        for data, p1, p2, turn in path:
+            print(f"{data} (P1: {p1}, P2: {p2}), Move: {turn}")
 
 
-def addPoints(node):
-    if node.bestmove % 2 == 1:
-        if node.data % 2 == 0:
-            node.p2points -= 1
-        else:
-            node.p1points += 1
-    else:
-        if node.data % 2 == 0:
-            node.p1points -= 1
-        else:
-            node.p2points += 1
+    def printTree(root):
+        for node in root.children:
+            # Labakais Gājiens priekš P1. Ja ir mīnusā skaitlis, tad ir slikts gājiens.
+            print("--" * node.turnCount + ">" + str(node.gameNum) +
+                  f" (P1: {node.p1points}, P2: {node.p2points}), Bestmove: {node.heuristic()}")
+            HeurTreeNode.printTree(node)
 
-
-def ShortWinP1(node, path=[]):
-    path.append((node.data, node.p1points, node.p2points, node.bestmove))
-    if node.data >= 1200 and node.p1points > node.p2points:
-        return path
-    
-    best_path = None
-    for child in sorted(node.children, key=lambda x: x.p1points - x.p2points, reverse=True):
-        new_path = ShortWinP1(child, path[:])
-        if new_path and (best_path is None or len(new_path) < len(best_path)):
-            best_path = new_path
-    
-    return best_path
-
-
-def printPath(path):
-    for data, p1, p2, turn in path:
-        print(f"{data} (P1: {p1}, P2: {p2}), Move: {turn}")
-
-
-def printTree(root):
-    for node in root.children:
-        # Labakais Gājiens priekš P1. Ja ir mīnusā skaitlis, tad ir slikts gājiens. 
-        print("--" * node.bestmove + ">" + str(node.data) + 
-              f" (P1: {node.p1points}, P2: {node.p2points}), Bestmove: {node.heuristic()}")
-        printTree(node)
+    def buildTree(startNumb, depth=3):
+        root = HeurTreeNode(startNumb, 0)
+        createdNodes = set()
+        GameTree.createLevel(root, createdNodes, depth)  # uzstādīts dzīļums - 3 līmeņi
+        return root
 
 
 if __name__ == '__main__':
-    Tree = buildTree(8)
-    short_path = ShortWinP1(Tree)
+    Tree = HeurTreeNode.buildTree(8)
+    short_path = HeurTreeNode.ShortWinP1(Tree)
 
-    printTree(Tree)
+    HeurTreeNode.printTree(Tree)
 
 if short_path:
     print("\nShort win for P1:")
-    printPath(short_path)
+    HeurTreeNode.printPath(short_path)
