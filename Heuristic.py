0bbfe960bbd82a7976cc38a92e22286267140205
Tomass Lošaks <144779910+TomassLosaks@users.noreply.github.com>
2025-03-31 19:30:32 +0300

    Heuristic.py

--- Heuristic.py
+++ Heuristic.py
@@ -1,42 +1,28 @@
-import GameTree
+from GameTree import TreeNode
 
 
-class HeurTreeNode(GameTree.TreeNode):
-	def __init__(self, startingNumber, p1Points, p2Points, turnCount=0):
-		super().__init__(startingNumber, p1Points, p2Points, turnCount)
-		self.bestmove = None
+class HeurTreeNode(TreeNode):
+    def __init__(self, startingNumber, p1Points=0, p2Points=0, depth=0):
+        super().__init__(startingNumber, p1Points, p2Points, depth)
+        self.bestmove = None
 
-	def bestMove(self):
-		 return round((3 * (self.p1points - self.p2points) + ((1200 - self.gameNum) * (self.p1points - self.p2points))) / 20, 2)  # Fromula, kura dod koeficentu. (uzlabota versija)
+    def bestMove(self):
+        return round((self.p1points - self.p2points) + ((1200 - self.gameNum))/50, 2) # Uzlabojam formulu un uztaisījam vieglāg. Tagad neizvada 0, bet izvada tikai normalus skaitļus
 
-	def printTree(self):
-		for node in self.children:
-			print("--" * node.turnCount + ">" + str(node.gameNum) + f" (P1: {node.p1points}, P2: {node.p2points}), Bestmove: {node.bestmove}")
-			node.printTree()
+    def printTree(self):
+        for node in self.children:
+            if node.gameNum <= 1200: # Neizvada skaitļus vairak par 1200 lai nepiesarņot, jo tur iet viss mīnusā tālāk
+                print("--" * node.turnCount + ">" + str(node.gameNum) +
+                      f" (P1: {node.p1points}, P2: {node.p2points}), Bestmove: {node.bestmove}")
+                node.printTree()
 
-	def addPoints(self):
-		super().addPoints()
-		self.bestmove = self.bestMove()
+    def addPoints(self):
+        super().addPoints()
+        self.bestmove = self.bestMove()
 
-
-"""
-Viss īsākais ceļš priekš P1. Nedzēsu arā, vien karši sakomentēju
-
-def ShortWinP1(node, path=[]):
-    path.append((node.data, node.p1points, node.p2points, node.bestmove))
-    if node.data >= 1200 and node.p1points > node.p2points:
-        return path
-    
-    best_path = None
-    for child in sorted(node.children, key=lambda x: x.p1points - x.p2points, reverse=True):
-        new_path = ShortWinP1(child, path[:])
-        if new_path and (best_path is None or len(new_path) < len(best_path)):
-            best_path = new_path
-    
-    return best_path
-"""
 
 if __name__ == '__main__':
-	Tree = HeurTreeNode(8)
-	Tree.generateLevel(5)
-	Tree.printTree()
+    Tree = HeurTreeNode(8, 0, 0)
+    Tree.generateLevel(5)
+    Tree.printTree()
+
